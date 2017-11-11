4229dfe152875fe305e9ef08731049c5e2d6fd9e
bluedrack28 <bluedrack28@gmail.com>
2017-11-11 16:47:04 +0100

    Les ajouts du fichier test.conf

---
+++ tests/testsPartie.js
@@ -0,0 +1,7 @@
+describe('Test morpion', function() {
+    it('La partie existe', function() {
+        let partie = new Partie();
+        console.log(partie);
+        expect(partie).toBeUndefined;
+    });
+});
