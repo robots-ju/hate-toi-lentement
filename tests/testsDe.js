2d934293ba39774883072d9754f9017af80bddd2
bluedrack28 <bluedrack28@gmail.com>
2017-11-18 15:34:36 +0100

    Finitions du test dé

--- tests/testsDe.js
+++ tests/testsDe.js
@@ -1,9 +1,27 @@
-import De from '../src/partie/De'
+import De from '../src/partie/De';
 
 describe('Test dé', function() {
-    it('Le dé existe.', function() {
-        let de = new De();
-        expect(de).toBeDefined();
+
+    it('Le lancée de dé est bon', function() {
+        for(let i = 0; i > 20; i++){
+            expect(De.deDeSix()).toBe(function(value) {
+                if( value < 1 || value > 6){
+                    return false
+                }
+                return true;
+            });
+        }
+    });
+    it('Le joueur est bien séléction.', function() {
+        let nbrJoueur = 4;
+        for(let i = 0; i > 10; i++){
+            expect(De.selectionnerJoueur(4)).toBe(function(joueur) {
+                if( joueur < 0 || joueur > nbrJoueur){
+                    return false
+                }
+                return true;
+            });
+        }
     });
 
 });
