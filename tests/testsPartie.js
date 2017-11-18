2f5c886c77250c2254a0fc5c5e50042256af7175
bluedrack28 <bluedrack28@gmail.com>
2017-11-18 16:11:28 +0100

    Ajout de tests sur partie

--- tests/testsPartie.js
+++ tests/testsPartie.js
@@ -19,10 +19,48 @@ describe('Test Partie', function() {
             return false;
         });
     })
+
+    it('La fonction deplacerUnPion() est définie', function(){
+        let partie = new Partie();
+        expect(partie.deplacerUnPion()).toBeDefined();
+    })
+
+    it('La function deplacerUnPion() renvoie une exception quand les deux cases ne sont pas valides', function(){
+        let partie = new Partie();
+        expect(partie.deplacerUnPion(
+            new Case(Couleur.Jaune, TypeCase.Case, 0),
+            new Case(Couleur.Bleu, TypeCase.Case, 0))
+        ).toThrowError();
+        expect(partie.deplacerUnPion(
+            new Case(Couleur.Jaune, TypeCase.Case, 0),
+            new Case(Couleur.Jaune, TypeCase.Case, 0))
+        ).toThrowError()
+        expect(partie.deplacerUnPion(
+            new Case(Couleur.Jaune, TypeCase.Case, 3),
+            new Case(Couleur.Jaune, TypeCase.Case, 0))
+        ).toThrowError()
+        expect(partie.deplacerUnPion(
+            new Case(Couleur.Jaune, TypeCase.Base, 0),
+            new Case(Couleur.Jaune, TypeCase.Base, 0))
+        ).toThrowError()
+        expect(partie.deplacerUnPion(
+            new Case(Couleur.Jaune, TypeCase.Arrivee, 0),
+            new Case(Couleur.Jaune, TypeCase.Arrivee, 0))
+        ).toThrowError()
+
+    });
+
+    it('Si la partie est terminée, la function partieTerminee() = true', function(){
+        let partie = new Partie();
+        // Cree une partie fictive
+        expect(partie.partieTerminee()).toEqual(true);
+    })
+
     it("Si la partie est terminée, il n'y a plus de joueur actuel.", function(){
         let partie = new Partie();
-        // Cree un partie fictive
+        // Cree une partie fictive
         expect(partie.joueurGagnant).toBeDefined();
         expect(partie.joueurActuel).toBeNull();
     })
+
 });
