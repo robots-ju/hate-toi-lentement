8fada0f8feee1c2e71151b530c845d216d471a4d
Xavier Stouder <xavier@stouder.io>
2017-11-18 15:31:12 +0100

    Fix dé

--- src/partie/De.js
+++ src/partie/De.js
@@ -1,6 +1,6 @@
 import ContratDe from '../contracts/De';
 
-export class De extends ContratDe {
+export default class De extends ContratDe {
     deDeSix(){
         return Math.floor(Math.random() * 6) + 1;
     }
