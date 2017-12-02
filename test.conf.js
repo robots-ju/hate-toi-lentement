c3466e93a14a91715aadf9ca170ab844fa359c9b
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-12-02 15:28:01 +0100

    Corrigé configuration tests

--- test.conf.js
+++ test.conf.js
@@ -69,7 +69,6 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
-    reporters: ['progress'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
